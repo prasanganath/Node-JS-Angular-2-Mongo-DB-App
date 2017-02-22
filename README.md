2e74aadd389bca52613af15fde56f3441923dfed
Isuru Abeywardana <youremail.address.com>
2017-02-22 06:04:59 +0530

    connected mongoDB to the application
    application uses the db mean-stack-app-db  database

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 MEAN STACK APPLICATION
+Installing mongoDB
+
+npm install mongodb --save
+
+make db in mongo 
+
+use mean-stack-app-db
+
+added a code to app to check whether mongo db is connected correctly
+
+MongoClient.connect("mongodb://localhost:27017/mean-stack-app-db", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
+
+
+dont forget to  insert  this on top of app
+
+var MongoClient = require('mongodb').MongoClient;
--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 var express = require('express')
+var MongoClient = require('mongodb').MongoClient;
 	var app = express()
 	app.get('/', function (req, res) {
   		res.send('Hello World!')
 	})
+	// Retrieve
+
+
+// Connect to the db
+MongoClient.connect("mongodb://localhost:27017/mean-stack-app-db", function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+  }
+});
 	app.listen(3000, function () {
   		console.log('Example app listening on port 3000!')
 	})
+
 
